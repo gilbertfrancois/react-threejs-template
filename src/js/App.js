92894312f330bc97d6adc83c557e7d4a9d5df207
rodogir <me@rodogir.com>
2022-09-06 16:31:15 +0200

    add basic state

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
 export function App() {
-    return <>Hello World</>;
+    const [sliderValue, setSliderValue] = useState(0);
+    const [isPlaying, setIsPlaying] = useState();
+    return (
+        <>
+            <input
+                type="range"
+                name="slider"
+                min="0"
+                max="10"
+                value={sliderValue}
+                onChange={(e) => setSliderValue(e.target.valueAsNumber)}
+            />
+            <button
+                type="button"
+                onClick={() => setIsPlaying((oldVal) => !oldVal)}
+            >
+                {isPlaying ? "⏸️" : "▶️"}
+            </button>
+        </>
+    );
 }
