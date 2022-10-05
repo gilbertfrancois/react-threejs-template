64a3e9a5e2dca832bb5edff70c6b1aeee0e1be8e
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2022-10-05 15:02:27 +0200

    Adds destructor for WebGL renderer and refactoring.

--- src/js/App.js
+++ src/js/App.js
@@ -2,68 +2,88 @@ import { useCallback, useEffect, useState } from "react";
 import { Sketch } from "./sketch";
 
 export function App() {
-    const [sliderValue, setSliderValue] = useState(0);
-    const [isPaused, setIsPaused] = useState(true);
+    const [sliderValue, setSliderValue] = useState(20);
+    const [isPaused, setIsPaused] = useState(false);
     return (
         <>
             <div className="controls">
-                <button
-                    type="button"
-                    onClick={() => setIsPaused((oldVal) => !oldVal)}
-                >
-                    {isPaused ? "▶️" : "⏸️"}
-                </button>
-                <input
-                    type="range"
-                    name="slider"
-                    min="0"
-                    max="10"
-                    value={sliderValue}
-                    onChange={(e) => setSliderValue(e.target.valueAsNumber)}
-                />
+                <div className="label-control-grid">
+                <div className="label-control-grid-item">
+                    <label htmlFor="loop">Render loop</label>
+                </div>
+                <div className="label-control-grid-item">
+                    <button
+                        id="loop"
+                        type="button"
+                        onClick={() => setIsPaused((oldVal) => !oldVal)}
+                    >
+                        {isPaused ? "Start render loop" : "Stop render loop"}
+                    </button>
+                </div>
+
+                <div className="label-control-grid-item">
+                    <label htmlFor="rotate_y">Rotation</label>
+                </div>
+                <div className="label-control-grid-item">
+                    <input
+                        id="rotate_y"
+                        type="range"
+                        name="slider"
+                        min="0"
+                        max="100"
+                        value={sliderValue}
+                        onChange={(e) => setSliderValue(e.target.valueAsNumber)}
+                    />
+                </div>
+                </div>
             </div>
-            <div className="sketches">
-                <ThreeScene
-                    paused={isPaused}
-                    sliderValue={sliderValue}
-                    className="border-blue"
-                />
-                <ThreeScene
-                    paused={isPaused}
-                    sliderValue={sliderValue}
-                    className="border-green"
-                />
-            </div>
+            <ThreeSketch
+                paused={isPaused}
+                sliderValue={sliderValue}
+                className="sketch"
+            />
         </>
     );
 }
 
-function ThreeScene({ paused, sliderValue, className }) {
-    const [scene, setScene] = useState();
+function ThreeSketch({ paused, sliderValue, className }) {
+    const [sketch, setSketch] = useState();
 
+    // Scene constructor
     const nodeRef = useCallback((node) => {
         if (node !== null) {
-            setScene(new Sketch({ dom: node }));
+            setSketch(new Sketch({ dom: node }));
         }
     }, []);
 
+    // Scene destructor
     useEffect(() => {
-        if (!scene) {
+        if (!sketch) {
+            return;
+        }
+        return () => {
+            sketch.destroy();
+        };
+    }, [sketch]);
+
+    // Passes state from React app to Sketch object.
+    useEffect(() => {
+        if (!sketch) {
             return;
         }
         if (paused) {
-            scene.stop();
+            sketch.stop();
         } else {
-            scene.play();
+            sketch.play();
         }
-    }, [paused, scene]);
+    }, [paused, sketch]);
 
     useEffect(() => {
-        if (!scene) {
+        if (!sketch) {
             return;
         }
-        scene.setSliderValue(sliderValue);
-    }, [sliderValue, scene]);
+        sketch.setSliderValue(sliderValue);
+    }, [sliderValue, sketch]);
 
     return <div ref={nodeRef} className={className} />;
 }
--- src/js/sketch.js
+++ src/js/sketch.js
@@ -6,10 +6,12 @@ import vertex from "../shader/vertex.glsl";
 
 export class Sketch {
     constructor(options) {
+        this.destroyed = false;
         this.sliderValue = 0;
         this.scene = new THREE.Scene();
         this.gui = new GUI();
         this.container = options.dom;
+        console.log(this.container);
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
         this.renderer = new THREE.WebGLRenderer();
@@ -35,34 +37,37 @@ export class Sketch {
             this.camera,
             this.renderer.domElement,
         );
-        this.time = 0;
 
         this.isPlaying = false;
 
         this.addObjects();
         this.resize();
         this.render();
+        this.resize_bound = this.resize.bind(this);
         this.setupResize();
         this.settings();
     }
 
     settings() {
-        this.settings = {
-            progress: 0,
-        };
-        this.gui.add(this.settings, "progress", 0, 1, 0.01);
+        // Enable the GUI element if you want to change values quickly during
+        // development and testing.
+        //
+        // this.settings = {
+        //     progress: 0,
+        // };
+        // this.gui.add(this.settings, "progress", 0, 1, 0.01);
     }
 
     setupResize() {
-        window.addEventListener("resize", this.resize.bind(this));
+        window.addEventListener("resize", this.resize_bound);
     }
 
     resize() {
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
-        this.renderer.setSize(this.width, this.height);
         this.camera.aspect = this.width / this.height;
         this.camera.updateProjectionMatrix();
+        this.renderer.setSize(this.width, this.height);
     }
 
     addObjects() {
@@ -103,8 +108,12 @@ export class Sketch {
 
     render() {
         if (!this.isPlaying) return;
+        // Enable the GUI element if you want to change values quickly during
+        // development and testing.
+        //
+        // this.gui;
         this.time += 0.05;
-        this.gui;
+        this.plane.rotation.y += this.sliderValue / 1000;
         this.material.uniforms.time.value = this.time;
         requestAnimationFrame(this.render.bind(this));
         this.renderer.render(this.scene, this.camera);
@@ -112,5 +121,12 @@ export class Sketch {
 
     setSliderValue(value) {
         this.sliderValue = value;
+    }
+
+    destroy() {
+        this.renderer.dispose();
+        this.camera = undefined;
+        window.removeEventListener("resize", this.resize_bound);
+        this.destroyed = true;
     }
 }
